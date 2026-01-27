5554136073f440537828ff07f8101386fdc1a69e
Tomas Taboada <tomastaboada27@gmail.com>
2026-01-27 20:12:47 -0300

    Add MVC Structure

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
+
+const healthRoutes = require("./routes/health.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "API funcionando 🚀" });
-});
+app.use("/api/health", healthRoutes);
 
 const PORT = process.env.PORT || 3000;
 
